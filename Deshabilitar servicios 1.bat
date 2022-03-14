4ed2c774e2fdfcbd5728e7c2311b399135edf327
vhoda <70240006+vhoda@users.noreply.github.com>
2022-03-14 18:02:22 -0300

    Add files via upload

---
+++ Deshabilitar servicios 1.bat
@@ -0,0 +1,17 @@
+sc config WSearch start= disabled
+sc config WMPNetworkSvc start= disabled
+sc config SNMPTRAP start= disabled
+sc config SCPolicySvc start= disabled
+sc config SCardSvr start= disabled
+sc config RemoteRegistry start= disabled
+sc config RpcLocator start= disabled
+sc config WPCSvc start= disabled
+sc config CscService start= disabled
+sc config napagent start= disabled
+sc config Netlogon start= disabled
+sc config MSiSCSI start= disabled
+sc config iphlpsvc start= disabled
+sc config TrkWks start= disabled
+sc config CertPropSvc start= disabled
+sc config PeerDistSvc start= disabled
+sc config bthserv start= disabled
